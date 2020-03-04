6b4d77216c01a6ffa34a79dbc33acbda0c078901
Hoang Tung Son <hoangtungson164@gmail.com>
2020-03-04 10:10:35 +0700

    waste

--- src/app/component/redirect-backend/redirect-backend.component.ts
+++ src/app/component/redirect-backend/redirect-backend.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import {RedirectBackendService} from "./service/redirect-backend.service";
 
 @Component({
   selector: 'app-redirect-backend',
@@ -8,12 +9,20 @@ import {Router} from '@angular/router';
 })
 export class RedirectBackendComponent implements OnInit {
 
+  private url: string;
+
   constructor(
       private router: Router,
-
+      private redirectBackendService: RedirectBackendService,
   ) { }
 
   ngOnInit() {
+    this.url = this.router.url;
+    this.redirectBackendService.postBank(this.url).subscribe(next => {
+      console.log('nothing');
+    }, error => {
+      console.log('nothing');
+    });
     window.location.href = 'https://localhost:3400/';
   }
 
--- src/app/component/redirect-backend/service/redirect-backend.service.ts
+++ src/app/component/redirect-backend/service/redirect-backend.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RedirectBackendService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+
+  postBank(url: string): Observable<any> {
+    return this.httpClient.post('https://localhost:3001/redirect', url);
+  }
 }
