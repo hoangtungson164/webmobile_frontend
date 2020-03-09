c1093b6ecf52d3c5829721ec8a2eaaa6a002046c
dthoanginfoplus <hoangdt@infoplus.com>
2020-03-09 15:17:57 +0700

    change URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  URL: 'https://localhost:3400',
-  // URL: 'https://192.168.22.179:3400',
+  // URL: 'https://localhost:3400',
+  URL: 'https://192.168.22.179:3400',
   // externalURL: 'https://192.168.22.179:3100/external'
 };
