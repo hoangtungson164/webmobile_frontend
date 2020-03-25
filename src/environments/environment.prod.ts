1cc7b4f9120d7f004441d117c7a904a1a616771e
luannt2410 <luannt@infoplusvn.com>
2020-03-25 09:46:14 +0700

    change port ip server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  URL: 'https://localhost:3400',
+  // URL: 'https://localhost:3200',
+  URL: 'https://103.112.124.153:3200',
   // URL: 'https://192.168.22.179:3400',
   // externalURL: 'https://192.168.22.179:3100/external'
 };
