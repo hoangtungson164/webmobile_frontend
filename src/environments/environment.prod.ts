72d9ee9685aa1dde53cd3b29c09543daf8b7e09e
luannt2410 <luannt@infoplusvn.com>
2020-03-25 09:53:10 +0700

    fix ip server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // URL: 'https://localhost:3200',
-  URL: 'https://103.112.124.153:3200',
+  URL: 'https://10.84.221.153:3200',
   // URL: 'https://192.168.22.179:3400',
   // externalURL: 'https://192.168.22.179:3100/external'
 };
