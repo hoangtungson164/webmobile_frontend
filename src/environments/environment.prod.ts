f5dcf64e312b2e8f29815a0adaf01f00489bb5b3
dthoanginfoplus <hoangdt@infoplus.com>
2020-03-31 17:10:31 +0700

    change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  URL: 'https://localhost:3400',
-  // URL: 'https://103.112.124.153:3201',
+  // URL: 'https://localhost:3201',
+  URL: 'https://103.112.124.153:3201',
   // URL: 'https://103.112.124.129:3201',
   // URL: 'https://192.168.22.179:3201',
   // externalURL: 'https://192.168.22.179:3301/external'
