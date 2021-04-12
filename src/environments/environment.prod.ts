16f79b521eb824cff414d6ed0c1bbc56edac907b
hi <hoang>
2021-04-12 11:49:46 +0700

    add branch for Develop server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   // URL: 'https://localhost:3201',
-  // URL: 'https://103.112.124.153:3201',
-  URL: 'https://mobile-infobox.nicegroup.com.vn:3200',
+  URL: 'https://103.112.124.153:3200',
+  // URL: 'https://mobile-infobox.nicegroup.com.vn:3200',
   // URL: 'https://192.168.22.179:3201',
   // externalURL: 'https://192.168.22.179:3301/external'
   WaitTime: 20000,
@@ -10,5 +10,5 @@ export const environment = {
   local : 'https://localhost:3201/',
   testingSV : 'https://103.112.124.153:3201/',
   prodSV : 'https://103.112.124.129:3200/',
-  URL_SOCKET: 'https://mobile-infobox.nicegroup.com.vn:3200'
+  URL_SOCKET: 'https://103.112.124.153:3200'
 };
